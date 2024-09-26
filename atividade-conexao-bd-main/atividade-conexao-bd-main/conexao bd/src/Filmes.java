48a76d5c188c028cc00c809d86befa8854bf6902
Anna Júlia do Prado <147573503+jujobs@users.noreply.github.com>
2024-09-25 21:30:25 -0300

    Add files via upload

---
+++ atividade-conexao-bd-main/atividade-conexao-bd-main/conexao bd/src/Filmes.java
@@ -0,0 +1,34 @@
+package cap12;
+public class Filmes {
+    private String codigo, titulo, genero, produtora, dataCompra;
+    public String getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    public String getDataCompra() {
+        return dataCompra;
+    }
+    public String setDataCompra(String dataCompra) {
+        this.dataCompra = dataCompra;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public String setGenero(String genero) {
+        this.genero = genero;
+    }
+    public String getProdutora() {
+        return produtora;
+    }
+    public String setProdutora(String produtora) {
+        this.produtora = produtora;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public String setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+}
