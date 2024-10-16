675afda4bd9ee6d5f4d77433a9e30ce1a20968f4
Jubs <2008.annajuh@gmail.com>
2024-10-15 23:32:07 -0300

    códigos comentados!!

--- atividade-conexao-bd-main/atividade-conexao-bd-main/conexao bd/src/Conecta.java
+++ atividade-conexao-bd-main/atividade-conexao-bd-main/conexao bd/src/Conecta.java
@@ -1,20 +1,19 @@
-package cap12;
+package cap12;   /* da linha 1 a 3 ficam os pacotes. aqui temos um novo pacote, que trabalha com o banco de dados. */
 import java.sql.*;
 import javax.swing.*;
-public class Conecta {
-    public static void main(String[] args) {
-        final String DRIVER = "com.mysql.jdbc.Driver";
-        final String URL = "jdbc:mysql://localhost:3306/mysql";
-        try {
-            Class.forName(DRIVER);
-            Connection connection = DriverManager.getConnection(URL, "root", "");
-
-
-        } catch (SQLException erro) {
-            JOptionPane.showMessageDialog(null, "Problemas na conexão com a fonte de dados\n"
+public class Conecta {   /* abrindo a classe Conecta */
+    public static void main(String[] args) {   /* começando o método principal da classe */
+        final String DRIVER = "com.mysql.jdbc.Driver";    /* cria uma variável para acessar o driver usado na conexão */
+        final String URL = "jdbc:mysql://localhost:3306/mysql";  /* cria uma variável para acessar o url do banco de dados */
+        try {   /* começo do método try catch - faz o sistema que indica o que fazer em caso de erro */
+            Class.forName(DRIVER);  /* chama o driver usado para fazer a conexão */
+            Connection connection = DriverManager.getConnection(URL, "root", "");  /* estabelece a conexão ao banco de dados,
+             colocando a url (inserida na variável), a senha e o usuário */
+        } catch (SQLException erro) { /* primeira mensagem de erro */
+            JOptionPane.showMessageDialog(null, "Problemas na conexão com a fonte de dados\n"  /* mensagem que aparece quando dá erro no banco de dados */
             + erro.toString());
-        } catch (ClassNotFoundException e) {
-            JOptionPane.showMessageDialog(null, "Driver não encontrado!\n"
+        } catch (ClassNotFoundException e) {  /* segunda mensagem de erro */
+            JOptionPane.showMessageDialog(null, "Driver não encontrado!\n"  /* mensagem de erro que aparece quando o driver não está instalado */
                     + e.toString());
         }
     }
--- atividade-conexao-bd-main/atividade-conexao-bd-main/conexao bd/src/ConsultaFilmes.java
+++ atividade-conexao-bd-main/atividade-conexao-bd-main/conexao bd/src/ConsultaFilmes.java
@@ -1,32 +1,33 @@
-package cap12;
+package cap12;  /* da linha 1 a 3 ficam os pacotes. aqui temos um novo pacote, que trabalha com o banco de dados. */
 import java.sql.*;
 import javax.swing.*;
-public class ConsultaFilmes {
-    public static void main(String[] args) {
-        final String DRIVER = "com.mysql.jdbc.Driver";
-        final String URL = "jdbc:mysql://localhost:3306/banco";
-        try {
-            Class.forName(DRIVER);
-            Connection connection = DriverManager.getConnection(URL, "root", "123456");
-            String sql = "SELECT codigo, titulo FROM Filmes Where codigo > ? AND codigo < ? ORDER BY codigo";
-            PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setString(1, "03120");
-            statement.setString(2, "03140");
-            ResultSet resultSet = statement.executeQuery();
-            System.out.println("CÓDIGO   TÍTULO");
+public class ConsultaFilmes {  /* abertura da classe ConsultaFilmes */
+    public static void main(String[] args) {  /* começa o método principal da classe */
+        final String DRIVER = "com.mysql.jdbc.Driver";  /* começando o método principal da classe */
+        final String URL = "jdbc:mysql://localhost:3306/banco"; /* cria uma variável para acessar o driver usado na conexão */
+        try {  /* começo do método try catch */
+            Class.forName(DRIVER);  /* acessa o driver que faz a conexão ao banco de dados */
+            Connection connection = DriverManager.getConnection(URL, "root", "123456");  /* faz a conexão com o banco de dados com a senha, url e usuário */
+            String sql = "SELECT codigo, titulo FROM Filmes Where codigo > ? AND codigo < ? ORDER BY codigo";  /* cria uma variavel de texto - é um comando na linguagem
+             sql, feito para selecionar o codigo e titulo da tabela Filmes os organizando pelo código */
+            PreparedStatement statement = connection.prepareStatement(sql);  /* prepara o comando sql para o executar */
+            statement.setString(1, "03120");  /* define o primeiro parametro */
+            statement.setString(2, "03140");  /* define o segundo parametro */
+            ResultSet resultSet = statement.executeQuery();  /* Consulta os resultados */
+            System.out.println("CÓDIGO   TÍTULO");  /* um titulo bonitinho que faz uma tabela */
             System.out.println("~~~~~~   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            while (resultSet.next()) {
-                String codigo = resultSet.getString("codigo");
-                String titulo = resultSet.getString("titulo");
+            while (resultSet.next()) {  /* funciona enquanto existir registros */
+                String codigo = resultSet.getString("codigo");  /* obtém valor da coluna codigo */
+                String titulo = resultSet.getString("titulo");  /* obtém valor da coluna  titulo */
                 System.out.println(codigo + "   " + titulo);
             }
             resultSet.close();
             statement.close();
-            connection.close();
-        } catch (ClassNotFoundException erro) {
+            connection.close();   /* as tres linhas passadas contando com essa fecham resultados, definições e conexão */
+        } catch (ClassNotFoundException erro) {  /* mensagem de erro para quando o driver não é encontrado */
             JOptionPane.showMessageDialog(null, "Driver não encontrado!\n"
             + erro.toString());
-        }catch (SQLException erro) {
+        }catch (SQLException erro) {  /* mensagem de erro para quando houver problemas no sql */
             JOptionPane.showMessageDialog(null, "Problemas na conexão com a fonte de dados\n"
             + erro.toString());
         }
